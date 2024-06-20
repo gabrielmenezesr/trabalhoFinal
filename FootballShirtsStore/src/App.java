0dbfd190ce713a7190562f2a5d48fc1fe9ebcd1b
Gabriel Menezes <163942114+gabrielmenezesr@users.noreply.github.com>
2024-06-20 10:00:05 -0300

    Delete FootballShirtsStore directory

--- FootballShirtsStore/src/App.java
+++
@@ -1,9 +0,0 @@
-import database.Database;
-import views.LoginView;
-
-public class App {
-    public static void main(String[] args) {
-        Database.createTables();
-        new LoginView();
-    }
-}
